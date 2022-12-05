bc58d1f26a7bbf34e62c496e0e8ce3b8e538e492
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2022-12-05 00:05:14 -0300

    Boop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+import HomeScreen from './HomeScreen';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [home, setHome] = useState(true);
+
+    const props = { 
+        home,
+        setHome,
+    }
+
+    return (
+    <>
+        {home ? <HomeScreen {...props}/> : <></>}
+    </>
+    );
 }
 
 export default App;
---
+++ src/HomeScreen.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function HomeScreen() {
+  return (
+    <>
+    <div className='navbar'>
+      <h1 className='navHome'>Hawai'i Surf</h1>
+      <div className='navbarRight'>
+        <h1 className='navbarItem'>Forecasts</h1>
+        <h1 className='navbarItem'>Favorites</h1>
+        <h1 className='navbarItem'>Login</h1>
+        <h1 className='navbarItem'>Sign Up</h1>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default HomeScreen;
